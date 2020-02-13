0152a5ed948bfc98968c1ee7d8a536488dc42c80
Valery <vpa3264@gmail.com>
2020-02-13 19:19:41 +0500

    Configured webpack and plugins

---
+++ webpack.config.js
@@ -0,0 +1,192 @@
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+
+const {CleanWebpackPlugin} = require('clean-webpack-plugin') //Каждый раз очищает папку dist, на случай если какие-то файлы были переименованы или перемещены
+const HtmlWebpackPlugin = require('html-webpack-plugin') //Плагин для работы с HTML, в основном нужен для автомитического встраивания скриптов
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') //Плагин для извлечения всех CSS файлов в отдельный файл при сборке
+const postcssSCSS = require('postcss-scss') //Плагин для PostCSS, позволяющий воспринимать синтаксис SCSS
+const autoprefixer = require('autoprefixer') //Автоматически добавляет префиксы для свойств Css
+const doiuse = require('doiuse') //Инофрмирует о несовместимости тех или иных CSS свойств с браузерами
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin') //Сжимает JS
+const StyleLintPlugin = require('stylelint-webpack-plugin') //Более строгая валидация CSS
+
+const pages = []
+fs
+	.readdirSync(__dirname + '/src/pages')
+	.filter((file) => {
+		return file.indexOf('base') !== 0;
+	})
+	.forEach((file) => {
+		pages.push(file.split('/', 2));
+	})
+
+const htmlPlugins = pages.map(fileName => new HtmlWebpackPlugin({
+	getData: () => {
+		try {
+			return JSON.parse(fs.readFileSync(`./src/pages/${fileName}/data.json`, 'utf8'))
+		} catch (e) {
+			console.warn(`data.json was not provided for page ${fileName}`)
+      		return {}
+		}
+	},
+	filename: `${fileName}.html`,
+	template: `./src/pages/${fileName}/${fileName}.pug`,
+	inject: 'body',
+	hash: true,
+}))
+
+const confMode = process.env.NODE_ENV
+const confPlugins = [new CleanWebpackPlugin(), new StyleLintPlugin()].concat(htmlPlugins)
+const confOptimization = {}
+const confDevServer = {}
+const confModule = {
+	rules: [
+		{
+			test: /\.js$/,
+    		loader: 'babel-loader',
+    		exclude: [
+    			/node_modules/,
+    		]
+		},
+		{
+			test: /\.pug$/,
+			loader: 'pug-loader',
+		},
+		{
+			test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+			loader: 'file-loader?name=[path][name].[ext]'
+		}
+	]
+}
+const confOutput = {
+	filename: '[name].bundle.js',
+	path: __dirname + "/dist",
+	chunkFilename: '[name].bundle.js',
+	publicPath: '/'
+}
+let confDevtool = false
+
+if(process.env.NODE_ENV === "production") {
+	confOutput.publicPath = './'
+	confPlugins.push(new MiniCssExtractPlugin())
+	confOptimization.minimizer = [
+		new UglifyJsPlugin({
+			uglifyOptions: {
+				mangle: true
+			}
+		})
+	]
+	confModule.rules.push({
+		test: /\.css$/,
+		use: [MiniCssExtractPlugin.loader, 'css-loader']
+	})
+	confModule.rules.push(
+		{
+			test: /\.scss$/,
+			use: [
+				MiniCssExtractPlugin.loader,
+				'css-loader',
+				{
+					loader: 'postcss-loader',
+					options: {
+						plugins: function () {
+							return [
+								autoprefixer({overrideBrowserslist: ['last 2 versions']}),
+							]
+						}
+					}
+				},
+				'sass-loader',
+				{
+					loader: 'postcss-loader',
+					options: {
+						syntax: postcssSCSS,
+						plugins: function () {
+							return [
+								doiuse({
+									browsers:['ie >= 11', 'last 2 versions'],
+									ignore: ['flexbox', 'rem', 'css-resize', 'css-masks', 'object-fit'],
+									ignoreFiles: ['**/normalize.css'],
+								})
+							]
+						}
+					}
+				}
+			]
+		}
+	)
+}
+
+if(process.env.NODE_ENV === "development") {
+	confDevtool = 'eval'
+	confOutput.pathinfo = true
+	confPlugins.push(new webpack.HotModuleReplacementPlugin())
+
+	confDevServer.inline = true
+	confDevServer.hot = true
+	confDevServer.contentBase = 'dist'
+	confDevServer.host = '0.0.0.0'
+	confDevServer.port = process.env.PORT
+	
+	confModule.rules.push({
+		test: /\.css/,
+		use: [
+			'style-loader',
+			'css-loader'
+		]
+	})
+	confModule.rules.push(
+		{
+			test: /\.scss$/,
+			use: [
+				'style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						importLoaders: 1
+					}	
+				},
+				{
+					loader: 'postcss-loader',
+					options: {
+						plugins: function () {
+							return [
+								autoprefixer({overrideBrowserslist: ['last 2 versions']}),
+							]
+						}
+					}
+				},
+				'sass-loader',
+				{
+					loader: 'postcss-loader',
+					options: {
+						syntax: postcssSCSS,
+						plugins: function () {
+							return [
+								doiuse({
+									browsers:['ie >= 11', 'last 2 versions'],
+									ignore: ['flexbox', 'rem', 'css-resize', 'css-masks', 'object-fit'],
+									ignoreFiles: ['**/normalize.css'],
+								})
+							]
+						}
+					}
+				}
+			]
+		}
+	)
+}
+
+const conf = {
+	entry: './src/index.js',
+	devtool: confDevtool,
+	output: confOutput,
+	module: confModule,
+	mode: process.env.NODE_ENV,
+	plugins: confPlugins,
+	optimization: confOptimization,
+	devServer: confDevServer
+}
+
+module.exports = conf
